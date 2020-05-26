3b119a175b77dbbba835bb489e47e625b4b0e764
lwrnc788 <lwrnc788@gmail.com>
2020-05-26 23:34:37 +0800

    code spacing changes

--- functions/Entries.js
+++ functions/Entries.js
@@ -1,6 +1,6 @@
 class Entries{
 
-//adders
+  //adders
 
   addRadioInput(lvl){
     this.lvl_rad_name = lvl;
@@ -16,18 +16,16 @@ class Entries{
     this.lvl_name = lvl;
 
     this.l_name = document.createElement("div");
-    this.l_name.innerHTML = this.lvl_name + "<br />";
+    this.l_name.innerHTML = this.lvl_name;
     this.l_name.setAttribute("class", "class_Entry class_Name");
-
   }
 
   addLevelDescription(desc){
     this.lvl_desc = desc;
 
     this.l_desc = document.createElement("div");
-    this.l_desc.innerHTML = this.lvl_desc + "<br>";
-    this.l_desc.setAttribute("class", "class_Entry class_desc");
-
+    this.l_desc.innerHTML = this.lvl_desc;
+    this.l_desc.setAttribute("class", "class_Entry class_desc class_descdiv");
   }
 
   addLevelID(id){
@@ -53,7 +51,7 @@ class Entries{
     this.l_diff.innerHTML = this.lvl_diff;
     this.l_diff.setAttribute("class", "class_Entry class_diff");
 
-	  this.l_diffIcon = document.createElement("img");
+	this.l_diffIcon = document.createElement("img");
     this.l_diffIcon.setAttribute("class", "class_Entry class_diffIcon");
     this.l_diffIcon.setAttribute("src", "./img/" + this.lvl_diff + ".png");
     this.l_diffIcon.setAttribute("height", "80px");
@@ -73,7 +71,6 @@ class Entries{
     this.l_likes.innerHTML = "Likes: " + this.lvl_likes;
 
     this.l_likes.setAttribute("class", "class_Entry class_likes");
-
   }
 
   addLength(length){
@@ -81,7 +78,6 @@ class Entries{
     this.l_length = document.createElement("div");
     this.l_length.innerHTML = "Length: " + this.lvl_length;
     this.l_length.setAttribute("class", "class_Entry class_length");
-
   }
 
   addStars(stars){
@@ -89,7 +85,6 @@ class Entries{
     this.l_stars = document.createElement("div");
     this.l_stars.innerHTML = "Stars: " + this.lvl_stars;
     this.l_stars.setAttribute("class", "class_Entry class_stars");
-
   }
 
   //setters
@@ -132,11 +127,13 @@ class Entries{
     }
 
     this.main = document.createElement("div");
-    this.main.setAttribute("id","id_" + this.lvl_name);
+    this.main.setAttribute("id", "id_" + this.lvl_name);
     this.main.setAttribute("class", "class_Div");
 
+	// radio input to select the level in the form
     this.addRadioInput(this.lvl_name);
-
+	this.main.appendChild(this.l_radio);
+	
 	// left div, contains difficulty icon and stats
    	this.l_statsdiv = document.createElement("div");
 	this.l_statsdiv.setAttribute("class", "class_Entry class_statsdiv");
@@ -164,7 +161,7 @@ class Entries{
 
 		if(this.LevelLengthVisible)
 		  this.l_statsdiv.appendChild(this.l_length);
-
+	
 	// top right div, contains level name and uploader/author
 	this.l_textdiv = document.createElement("div");
 	this.l_textdiv.setAttribute("class", "class_Entry class_textdiv");
@@ -177,29 +174,28 @@ class Entries{
 
 	// bottom right div, contains the description
 	this.main.appendChild(this.l_desc);
-
+	
 	// adds a cleared blank line in order to fix the floated divs
 	this.l_floatclear = document.createElement("br");
 	this.l_floatclear.setAttribute("clear", "both");
 	this.main.appendChild(this.l_floatclear);
-
-    element_id.appendChild(this.l_radio);
-    element_id.appendChild(this.main);
-
+	
+	element_id.appendChild(this.main);
   }
 
-  checkConflict(lvl){
-
-      this.scope = $(".class_Name");
+  checkConflict(lvl){ 
+    this.scope = $(".class_Name");
+	
     for(this.i = 0; this.i < this.scope.length; this.i++){
-
       this.str_index = this.scope[this.i].innerText.trim();
       this.str_param = lvl.trim();
       this.result = (String(this.str_index) === String(this.str_param));
+	  
       if(this.result === true){
         return true;
       }
     }
+	
     return false;
   }
 
