5210c6810bd08cbd16f6d571ffcb71d18c33735c
lwrnc788 <lwrnc788@gmail.com>
2020-06-03 23:45:32 +0800

    changed adding difficulty face to also fetch from GDBrowser API; separated appending the main div to its parent, from appending all children of the main div

--- functions/Entries.js
+++ functions/Entries.js
@@ -1,33 +1,20 @@
 class Entries{
-
-//adders
-
-  addRadioInput(lvl){
-    this.lvl_rad_name = lvl;
-    this.l_radio = document.createElement("input");
-    this.l_radio.setAttribute("id", "radio_" + this.lvl_rad_name);
-    this.l_radio.setAttribute("type", "radio");
-    this.l_radio.setAttribute("name", "vote");
-    this.l_radio.setAttribute("class", "class_Entry class_Radio");
-    this.l_radio.setAttribute("value", this.lvl_rad_name);
-  }
-
+	
+  //adders
   addLevelName(lvl){
     this.lvl_name = lvl;
 
     this.l_name = document.createElement("div");
-    this.l_name.innerHTML = this.lvl_name + "<br />";
-    this.l_name.setAttribute("class", "class_Entry class_Name");
-
+    this.l_name.innerHTML = this.lvl_name;
+    this.l_name.setAttribute("class", "class_Name");
   }
 
   addLevelDescription(desc){
     this.lvl_desc = desc;
 
     this.l_desc = document.createElement("div");
-    this.l_desc.innerHTML = this.lvl_desc + "<br>";
-    this.l_desc.setAttribute("class", "class_Entry class_desc");
-
+    this.l_desc.innerHTML = this.lvl_desc;
+    this.l_desc.setAttribute("class", "class_desc");
   }
 
   addLevelID(id){
@@ -35,7 +22,16 @@ class Entries{
 
     this.l_id = document.createElement("div");
     this.l_id.innerHTML = "ID: " + this.lvl_id;
-    this.l_id.setAttribute("class", "class_Entry class_id");
+    this.l_id.setAttribute("class", "class_id");
+  }
+  
+  addLevelDifficultyFace(diffFace){
+	this.lvl_diffFace = diffFace;
+	
+	this.l_diffFace = document.createElement("img");
+    this.l_diffFace.setAttribute("class", "class_diffFace");
+    this.l_diffFace.setAttribute("src", "http://gdbrowser.com/difficulty/" + this.lvl_diffFace + ".png");
+    this.l_diffFace.setAttribute("height", "80px");
   }
 
   addLevelAuthor(author){
@@ -43,7 +39,7 @@ class Entries{
 
     this.l_author = document.createElement("div");
     this.l_author.innerHTML = "by " + this.lvl_author;
-    this.l_author.setAttribute("class", "class_Entry class_author");
+    this.l_author.setAttribute("class", "class_author");
   }
 
   addLevelDifficulty(diff){
@@ -51,12 +47,7 @@ class Entries{
 
     this.l_diff = document.createElement("div");
     this.l_diff.innerHTML = this.lvl_diff;
-    this.l_diff.setAttribute("class", "class_Entry class_diff");
-
-	  this.l_diffIcon = document.createElement("img");
-    this.l_diffIcon.setAttribute("class", "class_Entry class_diffIcon");
-    this.l_diffIcon.setAttribute("src", "./img/" + this.lvl_diff + ".png");
-    this.l_diffIcon.setAttribute("height", "80px");
+    this.l_diff.setAttribute("class", "class_diff");
   }
 
   addDownload(dl){
@@ -64,7 +55,7 @@ class Entries{
     this.l_dl = document.createElement("div");
     this.l_dl.innerHTML = "Downloads: " + this.lvl_dl;
 
-    this.l_dl.setAttribute("class", "class_Entry class_dl");
+    this.l_dl.setAttribute("class", "class_dl");
   }
 
   addLikes(likes){
@@ -72,28 +63,28 @@ class Entries{
     this.l_likes = document.createElement("div");
     this.l_likes.innerHTML = "Likes: " + this.lvl_likes;
 
-    this.l_likes.setAttribute("class", "class_Entry class_likes");
-
+    this.l_likes.setAttribute("class", "class_likes");
   }
 
   addLength(length){
     this.lvl_length = length;
     this.l_length = document.createElement("div");
     this.l_length.innerHTML = "Length: " + this.lvl_length;
-    this.l_length.setAttribute("class", "class_Entry class_length");
-
+    this.l_length.setAttribute("class", "class_length");
   }
 
   addStars(stars){
     this.lvl_stars = stars;
     this.l_stars = document.createElement("div");
     this.l_stars.innerHTML = "Stars: " + this.lvl_stars;
-    this.l_stars.setAttribute("class", "class_Entry class_stars");
-
+    this.l_stars.setAttribute("class", "class_stars");
   }
 
   //setters
-
+  setLevelDifficultyFaceVisible(bool){
+    this.LevelDifficultyFaceVisible = bool;
+  }
+  
   setLevelIDVisible(bool){
     this.LevelIDVisible = bool;
   }
@@ -122,28 +113,21 @@ class Entries{
     this.LevelStarsVisible = bool;
   }
 
-
-  deploy(element_id){//deploy the div element.
+  preloadMain(){//deploy the div element.
     //console.log(this.lvl_name); //(Debug Purposes)
 
-    if(this.checkConflict(this.l_name.innerText)){
-      alert("This Level is already on the poll.");
-      return;
-    }
-
+	// main div for the whole entry
     this.main = document.createElement("div");
-    this.main.setAttribute("id","id_" + this.lvl_name);
-    this.main.setAttribute("class", "class_Div");
-
-    this.addRadioInput(this.lvl_name);
+    this.main.setAttribute("id", "id_" + this.lvl_name);
+    this.main.setAttribute("class", "class_Entry");
 
 	// left div, contains difficulty icon and stats
    	this.l_statsdiv = document.createElement("div");
-	this.l_statsdiv.setAttribute("class", "class_Entry class_statsdiv");
+	this.l_statsdiv.setAttribute("class", "class_statsdiv");
 	this.main.appendChild(this.l_statsdiv);
 
-		if(this.LevelDifficultyVisible) {
-		  this.l_statsdiv.appendChild(this.l_diffIcon);
+		if(this.LevelDifficultyFaceVisible) {
+		  this.l_statsdiv.appendChild(this.l_diffFace);
 		}
 
 		if(this.LevelIDVisible)
@@ -167,7 +151,7 @@ class Entries{
 
 	// top right div, contains level name and uploader/author
 	this.l_textdiv = document.createElement("div");
-	this.l_textdiv.setAttribute("class", "class_Entry class_textdiv");
+	this.l_textdiv.setAttribute("class", "class_textdiv");
 	this.main.appendChild(this.l_textdiv);
 
 		this.l_textdiv.appendChild(this.l_name);
@@ -182,25 +166,9 @@ class Entries{
 	this.l_floatclear = document.createElement("br");
 	this.l_floatclear.setAttribute("clear", "both");
 	this.main.appendChild(this.l_floatclear);
-
-    element_id.appendChild(this.l_radio);
-    element_id.appendChild(this.main);
-
   }
-
-  checkConflict(lvl){
-
-      this.scope = $(".class_Name");
-    for(this.i = 0; this.i < this.scope.length; this.i++){
-
-      this.str_index = this.scope[this.i].innerText.trim();
-      this.str_param = lvl.trim();
-      this.result = (String(this.str_index) === String(this.str_param));
-      if(this.result === true){
-        return true;
-      }
-    }
-    return false;
+  
+  addMain(element_id) {
+	element_id.appendChild(this.main);
   }
-
 }
